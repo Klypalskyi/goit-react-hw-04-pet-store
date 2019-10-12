8c35cd168660d65f5c5366001cf984c7c7f6b54e
Klypalskyi <klypalskyi@gmail.com>
2019-10-12 04:23:07 +0300

    Updates

---
+++ precache-manifest.b153e3b4d057ea76cfe4fc7600e60026.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "584ab26d1ec1663dfb98f7b21b6939db",
+    "url": "/goit-react-hw-04-pet-store/index.html"
+  },
+  {
+    "revision": "e1e79a3d0c9bc5fc89f3",
+    "url": "/goit-react-hw-04-pet-store/static/css/main.c58777d2.chunk.css"
+  },
+  {
+    "revision": "f246534b6e2d91543101",
+    "url": "/goit-react-hw-04-pet-store/static/js/2.513eab7a.chunk.js"
+  },
+  {
+    "revision": "e1e79a3d0c9bc5fc89f3",
+    "url": "/goit-react-hw-04-pet-store/static/js/main.3e481acc.chunk.js"
+  },
+  {
+    "revision": "c2f38be6ab2243fb0adb",
+    "url": "/goit-react-hw-04-pet-store/static/js/runtime~main.c5eb63ea.js"
+  }
+]);
