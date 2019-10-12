eff8a5ea863d7cdffb35239fe176e404d921ed5d
Klypalskyi <klypalskyi@gmail.com>
2019-10-12 04:34:35 +0300

    hw-04 pet-shop

--- src/components/Cat/Cat.jsx
+++ src/components/Cat/Cat.jsx
@@ -3,11 +3,7 @@ import styles from './Cat.module.css';
 
 const Cat = () => (
   <div className={styles.marco}>
-    {/* <div className={styles.cielo} /> */}
-    {/* <div className={styles.luna} /> */}
     <div className={styles.gato} />
-    {/* <div className={styles.muro} /> */}
-    {/* <div className={styles.edificios} /> */}
   </div>
 );
 export default Cat;
